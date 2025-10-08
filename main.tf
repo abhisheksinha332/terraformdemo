5ffcd100716974ddc8b67dc16bf06a99a6068b83
Abhishek Sinha (Bosch Global Software Technolo) <v-abhissinha@microsoft.com>
2025-10-08 15:58:36 +0530

    first commit

---
+++ main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.47.0"
+    }
+  }
+    backend {}
+    
+}
+
+
+provider "azurerm" {
+     client_id       = var.client_id
+    client_secret   = var.client_secret
+    subscription_id = var.subscription_id
+    tenant_id       = var.tenant_id
+  features {}
+}
+
+
+
+resource "azurerm_resource_group" "rg-app" {
+
+  name     = "my-app-rg-terraform"
+  location = "West US"
+}
+
---
+++ variables.tf
@@ -0,0 +1,22 @@
+variable name {
+  type        = string
+  description = "description"
+}
+
+variable client_id {
+  type        = string
+  description = "description"
+}
+variable client_secret {
+  type        = string
+  description = "description"
+}   
+variable subscription_id {
+  type        = string
+  description = "description"
+}
+
+variable tenant_id {
+  type        = string
+  description = "description"
+}
